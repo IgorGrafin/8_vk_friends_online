127b5a9f8ef298ce0ef15c0014f85c9b84d7599e
Igor Grafin <apaticmail@gmail.com>
2018-02-19 23:07:42 +0300

    initial

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,16 @@
 import vk
+import getpass
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6379260
 
 
 def get_user_login():
-    pass
+    return input("Введите логин: ")
 
 
 def get_user_password():
-    pass
+    return getpass.getpass("Введите пароль: ")
 
 
 def get_online_friends(login, password):
@@ -17,16 +18,29 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope="friends"
     )
     api = vk.API(session)
-    # например, api.friends.get()
+
+    return api.users.get(
+        user_ids=api.friends.getOnline(),
+        fields='first_name, last_name, online'
+    )
 
 
 def output_friends_to_console(friends_online):
-    pass
+    for friend in friends_online:
+        print("{0} {1}".format(
+            friend["first_name"],
+            friend["last_name"]
+        ))
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
+    try:
+        friends_online = get_online_friends(login, password)
+    except vk.exceptions.VkAuthError:
+        exit("Неверные логин/пароль")
     output_friends_to_console(friends_online)
